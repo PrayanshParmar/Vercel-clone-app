ba8f5d5570818cfedcf3d7729beefe757cc607c6
prayansh <parmarprayansh2@gmail.com>
2024-02-12 14:30:36 +0530

    added Dynamic region

--- vercel-deploy-service/config/s3.connector.js
+++ vercel-deploy-service/config/s3.connector.js
@@ -1,7 +1,7 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
-  region: "ap-south-1",
+  region: String(process.env.REGION),
   credentials: {
     accessKeyId: String(process.env.AWS_VERCEL_ACCESS_KEY),
     secretAccessKey: String(process.env.AWS_VERCEL_SECRET_ACCESS_KEY),
